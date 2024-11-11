1b14d7253ea297f00a570f09352e5e08c3c448a6
Edgar-ogannesian <113892627+Edgar-ogannesian@users.noreply.github.com>
2024-11-11 17:32:22 -0500

    Update one.cpp

--- one.cpp
+++ one.cpp
@@ -10,6 +10,25 @@ void count() {
     cout<<count(lis.cbegin(),lis.cend(),2);
 }
 
+void accumulateQest(){
+    vector<int> vec{4,3,2,4,5,2,1,3,5};
+    accumulate(vec.cbegin(),vec.cend(),0);
+
+}
+
+void elimDups(vector<string> &words) {
+    sort(words.begin(), words.end());
+
+    auto end_unique=unique(words.begin(),words.end());
+    words.erase(end_unique, words.end());
+}
+
+void fillQest(){
+     vector<int> vec{4,3,2,4,5,2,1,3,5};
+     fill_n(vec.begin(),vec.size(),0);
+     cout<<vec[3];
+}
+
 
 int main() {
     cout << "Hello, World!" << std::endl;
