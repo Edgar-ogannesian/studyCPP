73e03edcc036fa2525d9988d34f684d971998c4c
sfomenko <sfomenko@ptsecurity.com>
2024-12-01 13:08:14 +0300

    added description

--- 12.4.cpp
+++ 12.4.cpp
@@ -1,7 +1,10 @@
 #include <vector>
 #include <iostream>
 #include <unordered_set>
+#include <array>
 
+// Дан массив чисел и число - сумма двух элементов. Найти любые два элемента,
+// сумма которых равна введенному числу
 
 //1	2,1	8,2
 //2	1,1	3,1
@@ -12,16 +15,16 @@
 //7	6,1	8,1
 //8	7,1	1,2
 
-std::vector<int> twoSum(int arr[],int t)
+std::vector<int> twoSum(std::vector<int> arr,int t)
 {
     std::unordered_set<int> arrHash;
     std::vector<int> result;
-    for(int i=0;i<sizeof(arr);i++)
+    for(int i=0;i<arr.size();i++)
     {
         arrHash.emplace(arr[i]);
     }
 
-    for(int i=0;i<sizeof(arr);i++)
+    for(int i=0;i<arr.size();i++)
     {
         int y=t-arr[i];
 
@@ -35,7 +38,7 @@ std::vector<int> twoSum(int arr[],int t)
 }
 
 int main() {
-    int arr[]{4,1,2,3,7,8,9,10,11,14};
+    std::vector<int> arr{4,1,2,3,7,8,9,10,11,14};
 
     std::vector<int> result=twoSum(arr,5);
     for(int i:result)
