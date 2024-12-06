53a13a08786cdab231bf87dad17a0cac2a43e57d
sfomenko <sfomenko@ptsecurity.com>
2024-12-06 18:43:34 +0300

    improve merge sort

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -3,46 +3,56 @@
 #include <algorithm>
 #include <cassert>
 
-int mergeAndCount(std::vector<int>& arr, int left, int right) {
-    int mid=left+(right-left)/2;
-    int n1=mid-left+1;
-    int n2=right-mid;
+int mergeAndCount(std::vector<int>& arr, int left, int mid, int right) {
+    std::vector<int> tmp;
+    tmp.reserve(right - left);
 
-    std::vector<int> L(n1), R(n2);
+    std::copy(arr.begin() + left, arr.begin() + right, std::back_inserter(tmp));
 
-    std::copy(arr.begin()+left, arr.begin()+mid+1, L.begin());
-    std::copy(arr.begin()+mid+1, arr.begin()+right+1, R.begin());
+    int tmp_mid = mid - left;
 
-    int i=0, j=0, k=left;
-    int inv_count=0;
+    int i = 0;
+    int j = tmp_mid;
+    int k = left;
+    int inv_count = 0;
 
-    while (i<n1 && j<n2) {
-        if (L[i]<=R[j]) {
-            arr[k]=L[i];
+    while (i < tmp_mid && j < tmp.size())
+    {
+        if (tmp[i] <= tmp[j])
+        {
+            arr[k] = tmp[i];
             i++;
-        } else {
-            arr[k]=R[j];
-            inv_count+=(n1-i);
+        }
+        else
+        {
+            arr[k] = tmp[j];
+            inv_count += (tmp_mid - i);
             j++;
         }
         k++;
     }
 
-    std::copy(L.begin()+i,L.end(),arr.begin()+k);
-    std::copy(R.begin()+j,R.end(),arr.begin()+k+n1-i);
+    std::copy(tmp.begin() + i,tmp.begin() + tmp_mid,arr.begin() + k);
+    k += tmp_mid - i;
+    std::copy(tmp.begin() + j, tmp.begin() + tmp_mid, arr.begin() + k);
 
     return inv_count;
 }
 
-int mergeSortAndCount(std::vector<int>& arr, int left, int right) {
-    int inv_count=0;
+int mergeSortAndCount(std::vector<int>& arr, int left, int right)
+{
+    int inv_count = 0;
 
-    if (left<(right-1)) {
-        int mid=left+(right-1 - left)/2+1;
-        inv_count+=mergeSortAndCount(arr,left,mid);
-        inv_count+=mergeSortAndCount(arr, mid,right);
-        inv_count+=mergeAndCount(arr, left, right-1);
+    if (right - left < 2)
+    {
+        return inv_count;
     }
+
+    int mid = left + (right - left) / 2;
+    inv_count += mergeSortAndCount(arr, left, mid);
+    inv_count += mergeSortAndCount(arr, mid, right);
+    inv_count += mergeAndCount(arr, left, mid, right);
+
     return inv_count;
 }
 
