58a68029be53ceb0c4040a754aa97d3e0ebaa6a8
donsimon <ogannesanedgar78@gmail.com>
2024-12-07 11:18:34 -0500

    fixed build and added cmake

--- binSearch.cpp
+++ binSearch.cpp
@@ -1,54 +1,65 @@
 #include <iostream>
 #include <vector>
 #include <cassert>
-#include <optional>
+#include<cmath>
 
-std::optional<unsigned> binarySearch(std::vector<int>& arr,int num){
-    int left=0,right=arr.size()-1;
+template<typename T>
+typename std::vector<T>::iterator binarySearch(std::vector<T>& arr,T num){
+    int n=arr.size()/2 + arr.size()%2;
+    int flag=0;
+    typename std::vector<T>::iterator pointer=arr.begin() + arr.size()/2 - (arr.size()%2==0);
 
-    while(left<=right){
-        int mid=left+(right-left)/2;
-
-        if(arr[mid]==num)
-        {
-            return mid+1;
-        }
-        if(arr[mid]<num)
-        {
-            left=mid+1;
-        }else {
-            right=mid-1;
-        }
+    if(arr.empty())
+    {
+        return arr.end();
     }
-    return std::nullopt;
+
+    do{
+        flag++;
+        n=n/2 + (n%2) - (n==3);
+
+        if(*pointer==num)
+        {
+            return pointer;
+        }else if(num>*pointer && pointer!=arr.end())
+        {
+            pointer+=n;
+        }else if(num<*pointer && pointer!=arr.begin())
+        {
+            pointer-=n;
+        }
+    }while(flag<std::log2(arr.size()));
+
+    return arr.end();
 }
 
-bool testBinarySerch(std::vector<int>arr,int num)
+template<typename T>
+bool testBinarySerch(typename std::vector<T>arr,T num)
 {
-    std::optional<unsigned> expexted=std::nullopt;
-    auto result=binarySearch(arr,num);
+    typename std::vector<T>::iterator expexted=arr.begin();
+    typename std::vector<T>::iterator result=binarySearch(arr,num);
 
-    for(int i=0;i<arr.size();i++)
+    while (expexted!=arr.end())
     {
-        if(arr[i]==num)
+        if(num==*expexted)
         {
-            expexted=i+1;
             break;
         }
+        expexted++;
     }
 
     return expexted==result;
 }
 
 int main(){
-    std::vector<int> arr={1,2,3,4,5,6,7,8,9};
 
     assert(testBinarySerch(std::vector<int>{},6));
     assert(testBinarySerch(std::vector<int>{1},1));
     assert(testBinarySerch(std::vector<int>{1},6));
-    assert(testBinarySerch(std::vector<int>{-4,-3,0,6,7,8,9},-3));
+    assert(testBinarySerch(std::vector<int>{-4,-3,0,6,7,8,9},-9));
     assert(testBinarySerch(std::vector<int>{1,2,3,4,5,6,7,8,9},0));
-    assert(testBinarySerch(std::vector<int>{1,2,3,4,5,6,7,8,9},6));
+    assert(testBinarySerch(std::vector<int>{1,2,3,4,5,7,8,9},7));
+    assert(testBinarySerch(std::vector<double>{1.4,2.4,3.3,4.2,5.1,6.1,7.5,8.43,9.2},6.8));
 
     return 0;
 }
